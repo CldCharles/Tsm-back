97aa5dd81a75ff01e8997a8bb080ea7a9d243a0e
charles-claude <charles.claude@epitech.eu>
2024-12-08 10:58:24 +0100

    init

---
+++ src/main/java/com/example/tsmback/TsmBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.tsmback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsmBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsmBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tsmback/TsmBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tsmback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsmBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
